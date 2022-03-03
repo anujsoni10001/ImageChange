e93910264a00ecadfa71c82893814d61c8f0ebbf
Anuj Soni <anujsoni10001@gmail.com>
2022-03-03 14:09:07 +0530

    Changed Name of Project to ImageChange

--- MyApp1/ViewController.swift
+++ MyApp1/ViewController.swift
@@ -5,15 +5,23 @@
 //  Created by Anuj Soni on 02/03/22.
 //
 
+//Button action to change im
+
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var image: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func Click(_ sender: UIButton) {
+        image.image = UIImage.init(named:"helianthus-yellow-flower-pixabay_11863.jpeg")
+    }
+    
 }
 
